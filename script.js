317a80b2d3f0f65eb5fd4af664fe5726cb35a3b9
rp-02 <rpackham75@gmail.com>
2023-07-04 14:39:05 -0700

    array for gameboard

---
+++ script.js
@@ -0,0 +1,28 @@
+// You’re going to store the gameboard as an array inside of a Gameboard object,
+// so start there! Your players are also going to be stored in objects, 
+// and you’re probably going to want an object to control the flow of the game itself. 
+
+let gameboard = 
+// create array 
+const gameboard = (() => {
+    let gameboard = ["-","-","-",
+                     "-","-","-",
+                     "-","-","-"]
+    // gameboard array index
+    // [0,1,2,
+    //  3,4,5,
+    //  6,7,8]
+    // possible solutions -
+    // Across
+    // 012
+    // 345
+    // 678
+    // Down
+    // 036
+    // 147
+    // 258
+    // Diagonal
+    //048
+    //246
+  })();
+  
